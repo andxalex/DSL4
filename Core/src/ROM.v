f15402d55ba30a43715b9e5e5fe26bdc53b4f1ef
andxalex <andreas.so.alexandrou@gmail.com>
2024-03-01 13:28:44 +0000

    format rom again

--- Core/src/ROM.v
+++ Core/src/ROM.v
@@ -1,15 +1,20 @@
 module ROM (
     //standard signals
-    input CLK,
+    input            CLK,
     //BUS signals
     output reg [7:0] DATA,
-    input [7:0] ADDR
+    input      [7:0] ADDR
 );
+
   parameter RAMAddrWidth = 8;
+
   //Memory
   reg [7:0] ROM[2**RAMAddrWidth-1:0];
+
   // Load program
   initial $readmemh("Complete_Demo_ROM.txt", ROM);
+
   //single port ram
   always @(posedge CLK) DATA <= ROM[ADDR];
+
 endmodule
