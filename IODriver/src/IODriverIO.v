4838eea95d7a02b5f2550a9ff136c8402bd34a9e
andxalex <andreas.so.alexandrou@gmail.com>
2024-03-17 18:19:52 +0000

    Cleaned up IO

--- IODriver/src/IODriverIO.v
+++ IODriver/src/IODriverIO.v
@@ -2,20 +2,18 @@
 
 
 module IODriverIO (
-    input         CLK,
-    input         RESET,
+    input        CLK,
+    input        RESET,
     // CTRL
-    input         BTN_L,
-    input         BTN_R,
-    input         BTN_U,
-    input         BTN_D,
-    input  [15:0] SLIDE_S,
+    input        BTN_L,
+    input        BTN_R,
+    input        BTN_U,
+    input        BTN_D,
+    input [15:0] SLIDE_S,
     // BUS
-    inout  [ 7:0] BUS_DATA,
-    inout  [ 7:0] BUS_ADDR,
-    input         BUS_WE,
-    // Additional
-    output  [15:0] test
+    inout [ 7:0] BUS_DATA,
+    inout [ 7:0] BUS_ADDR,
+    input        BUS_WE
 );
 
   // Wires
@@ -26,31 +24,31 @@ module IODriverIO (
 
   // Debounce and detect negative edges.
   NegEdgeDetector btn_r (
-    .CLK(CLK),
-    .RESET(RESET),
-    .IN(BTN_R),
-    .OUT(r_nedge_deb)
+      .CLK(CLK),
+      .RESET(RESET),
+      .IN(BTN_R),
+      .OUT(r_nedge_deb)
   );
 
   NegEdgeDetector btn_l (
-    .CLK(CLK),
-    .RESET(RESET),
-    .IN(BTN_L),
-    .OUT(l_nedge_deb)
+      .CLK(CLK),
+      .RESET(RESET),
+      .IN(BTN_L),
+      .OUT(l_nedge_deb)
   );
 
   NegEdgeDetector btn_u (
-    .CLK(CLK),
-    .RESET(RESET),
-    .IN(BTN_U),
-    .OUT(u_nedge_deb)
+      .CLK(CLK),
+      .RESET(RESET),
+      .IN(BTN_U),
+      .OUT(u_nedge_deb)
   );
 
   NegEdgeDetector btn_d (
-    .CLK(CLK),
-    .RESET(RESET),
-    .IN(BTN_D),
-    .OUT(d_nedge_deb)
+      .CLK(CLK),
+      .RESET(RESET),
+      .IN(BTN_D),
+      .OUT(d_nedge_deb)
   );
 
   // Also debouncing for side switches
@@ -58,12 +56,12 @@ module IODriverIO (
 
   genvar j;
   generate
-    for (j=0; j < 16; j=j+1) begin
+    for (j = 0; j < 16; j = j + 1) begin
       BtnDebounce slide_s (
-        .CLK(CLK),
-        .RESET(RESET),
-        .BTN(SLIDE_S[j]),
-        .DB_BTN(slide_switches[j])
+          .CLK(CLK),
+          .RESET(RESET),
+          .BTN(SLIDE_S[j]),
+          .DB_BTN(slide_switches[j])
       );
     end
   endgenerate
@@ -85,30 +83,29 @@ module IODriverIO (
 
   // Buffer 
   assign BufferedBusData = BUS_DATA;
-  
+
   integer i;
   always @(posedge CLK) begin
     if (RESET) begin
       DataBusOutWE <= 1'b0;
-      for (i=0; i<3; i=i+1) regBank[i] <= 8'h00;
+      for (i = 0; i < 3; i = i + 1) regBank[i] <= 8'h00;
     end else begin
       // Small trick to maintain button status until it is lowered by the processor
       regBank[0] <= {4'h0, r_nedge_deb, l_nedge_deb, u_nedge_deb, d_nedge_deb} | regBank[0];
-      
+
       // Slide switches are permanent, can't be modified by processor.
-      regBank[1] <= slide_switches[7:0]; 
-      regBank[2] <= slide_switches[15:8]; 
+      regBank[1] <= slide_switches[7:0];
+      regBank[2] <= slide_switches[15:8];
 
       // This assignment takes priority over the one above.
       if ((BUS_ADDR >= BaseAddr) & (BUS_ADDR < (BaseAddr + 3))) begin
         if (BUS_WE) begin
           DataBusOutWE <= 1'b0;
-          if (BUS_ADDR == BaseAddr) regBank[BUS_ADDR - BaseAddr] <= BufferedBusData;
+          if (BUS_ADDR == BaseAddr) regBank[BUS_ADDR-BaseAddr] <= BufferedBusData;
         end else DataBusOutWE <= 1'b1;
       end else DataBusOutWE <= 1'b0;
     end
     DataBusOut <= regBank[BUS_ADDR-BaseAddr];
   end
 
-  assign test = {regBank[2],4'h0,regBank[0][3:0]};
 endmodule
